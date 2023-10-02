f54a36ba9598226d54adffa4ead0aebc247b1d2d
kisia0916 <fumiharuabe@gmail.com>
2023-10-02 19:20:10 +0900

    homeのuiを変更しました

--- src/components/Thread/ThreadMain.tsx
+++ src/components/Thread/ThreadMain.tsx
@@ -3,6 +3,7 @@ import "./ThreadMain.css"
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ThreadTagMain from './ThreadTag/ThreadTagMain';
+import PersonIcon from '@mui/icons-material/Person';
 function ThreadMain(props:{topFlg?:boolean}) {
   let radStyle:string = ""
   if(props.topFlg){
@@ -22,15 +23,19 @@ function ThreadMain(props:{topFlg?:boolean}) {
         </div>
         <div className='ThreadMainRightBottom'>
           <img src='/photos/zbnU2dcD_400x400.jpg'  alt='logo' className='ThreadCreateUserIcon'/>
-          <span className='ThreadCreateUserName'>fumi</span>
+          <span className='ThreadCreateUserName'>fumi - 2023/10/2</span>
           <div className='ThreadMainLike'>
-              <FavoriteIcon className='ThreadMainLikeIcon' style={{fontSize:"140%"}}/>
+              <FavoriteIcon className='ThreadMainLikeIcon' style={{fontSize:"130%"}}/>
               <span className='ThreadMainLikeNum'>12</span>
           </div>
           <div className='ThreadMainMess'>
               <ChatBubbleIcon className='ThreadMainMessIcon' style={{fontSize:"140%"}}/>
               <span className='ThreadMainMessNum'>31</span>
               
+          </div>
+          <div className='ThreadMainPerson'>
+              <PersonIcon className='ThreadMainPersonIcon' style={{fontSize:"160%"}}/>
+              <span className='ThreadMainPersonText'>10</span>
           </div>
         </div>
       </div>
