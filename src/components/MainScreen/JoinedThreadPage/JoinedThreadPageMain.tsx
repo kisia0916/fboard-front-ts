b2279c2ca4533750d6b723107ae28e62f16d08de
kisia0916 <fumiharuabe@gmail.com>
2024-02-13 22:04:32 +0900

    joinThreadのデータ取得の実装

--- src/components/MainScreen/JoinedThreadPage/JoinedThreadPageMain.tsx
+++ src/components/MainScreen/JoinedThreadPage/JoinedThreadPageMain.tsx
@@ -1,9 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./JoinedThreadPageMain.css"
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ThreadMain from "../../Thread/ThreadMain";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 function JoinedThreadPageMain(){
+    const [cookies,setCookie] = useCookies()
+    const [joinThreads,setJoinThreads] = useState<any>([])
+    const [loadDoneJoinFlg,setLoadDoneJoinFlg] = useState<boolean>(false)
+    useEffect(()=>{
+        axios.post("http://localhost:5000/thread/data/getjointhread",{
+            userId:cookies.userId,
+            pass:cookies.pass.toString(),
+            page:0
+        }).then((res)=>{
+            setJoinThreads(res.data)
+            setLoadDoneJoinFlg(true)
+        }).catch((error:any)=>{
+            console.log(error)
+        })
+    },[])
     return(
         <div className="JoinThreadPageMain">
             <div className="JoinThreadPageTop">
@@ -11,8 +28,9 @@ function JoinedThreadPageMain(){
                 <span className='firstListTitle'>Join Thread</span>
             </div>
             <div className="JoinThreadPageScreen">
-            <ThreadMain threadTitle='自作PCにつけられるグラボを探そう' threadId='test' joinNum={10} userNum={30} postNum={30} createUserName='kisia' createdDate='2-2-2t' tagList={["自作PC","GPU","雑談"]} titleIcon='http://localhost:5000/public/img/b379be33-d3d1-4c9b-afd4-4513a5d88720.jpg' userIcon='' isJoined={false}/>
-
+                {loadDoneJoinFlg?joinThreads.map((i:any)=>{
+                    return <ThreadMain threadTitle={i.title} threadId={i.theradId} joinNum={i.joinNum} userNum={10} postNum={i.messNum} createUserName={i.userName} createdDate={i.createdAt} tagList={i.tags} titleIcon={i.threadPhoto} userIcon='' isJoined={true}/>
+                }):<span>loading</span>}
             </div>
         </div>
     )
